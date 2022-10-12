473dfc2a370a34f1091d0bd29bea74231fcb2d52
sty00A4 <99866356+sty00A4@users.noreply.github.com>
2022-10-12 14:54:15 +0200

    demos in library

--- libs/gui.lua
+++ libs/gui.lua
@@ -166,6 +166,27 @@ return {
             end
         },{ __name = "gui.page" })
     end,
+    demos = {
+        buttonHunt = function()
+            return gui.app(
+                {
+                    main = gui.page({
+                        gui.button(
+                            gui.position.absolute(1, 1),
+                            gui.text("button", colors.white, colors.black),
+                            function(self, page, app)
+                                local w, h = term.getSize()
+                                self.components.position.x = math.random(1, w - #self.components.text.content - 1)
+                                self.components.position.y = math.random(1, h)
+                            end
+                        ),
+                    })
+                },
+                "main",
+                { version = "0.1", __TERMINATE = function(_) os.shutdown() end }
+            )
+        end
+    },
     metatype = metatype,
     expect = expect,
     range = range,
--- main.lua
+++ main.lua
@@ -1,21 +1,5 @@
 require("libs.ext")
 expect("gui", _G.gui, "table")
 term.reset()
-local app = gui.app(
-    {
-        main = gui.page({
-            gui.button(
-                gui.position.absolute(1, 1),
-                gui.text("button", colors.white, colors.black),
-                function(self, page, app)
-                    local w, h = term.getSize()
-                    self.components.position.x = math.random(1, w - #self.components.text.content - 1)
-                    self.components.position.y = math.random(1, h)
-                end
-            ),
-        })
-    },
-    "main",
-    { version = "0.1", __TERMINATE = function(_) os.shutdown() end }
-)
+local app = gui.demos.buttonHunt()
 app:loop()
